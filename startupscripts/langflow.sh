c2ba199cb2d779245e4f2c6ae9e71c2f2401c89e
Jakob Engdahl <jakobengdahl@gmail.com>
2024-06-27 10:47:09 +0200

    updates on langflow init

--- startupscripts/langflow.sh
+++ startupscripts/langflow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-conda create -n langflow_env_py310 python=3.10 
+conda create -n langflow_env_py310 python=3.10 -y 
 conda activate langflow_env_py310
 python -m pip install langflow -U
-python -m langflow run --PORT 9797 --HOST --host $(hostname -I) &
+python -m langflow run --no-open-browser --port 9797 --host $(hostname -I) &
